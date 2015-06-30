460487a3a65272b14b395c31b06f647ea64ecdeb
yjjewg11 <yjjewg11>
2015-06-30 18:34:17 +0800

    u

--- src/java/com/company/news/springMVC/mappingView/DateMappingJackson2JsonView.java
+++
@@ -1,7 +0,0 @@
-package com.company.news.springMVC.mappingView;
-
-import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
-
-public class DateMappingJackson2JsonView extends MappingJackson2JsonView {
-
-}
